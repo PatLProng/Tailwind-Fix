63bccb4ec353f484cd2d29e8cb091542929422c6
PatLProng <wred7werd@gmail.com>
2024-12-03 23:36:43 -0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import ReactLogo from './assets/reactlogo.png'
+
+export default function Header() {
+    return (
+
+    <main className="header">
+        <img className="reactimg" src={ReactLogo} alt="React Logo"/> 
+        <p className="text-slate-500">React</p>
+    
+    </main>
+    
+    
+        
+        
+    )
+}
